a505333c6a385eaa216ff27d8365d3fcc317ac4e
Lemasu <alex.schaub@lernende.bbw.ch>
2021-05-26 11:00:56 +0200

    Header von Main ergänzt.

--- src/main/java/ch/bbw/consolecalculator/App.java
+++ src/main/java/ch/bbw/consolecalculator/App.java
@@ -1,13 +1,15 @@
 package ch.bbw.consolecalculator;
 
 /**
- * Hello world!
+ * Console Calculator App
+ * @author Alex Schaub
+ * @version 0.0.1
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
