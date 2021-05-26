b11c25a21ad9c178eedbb2dfe3e8a93b7c037516
Lemasu <alex.schaub@lernende.bbw.ch>
2021-05-26 11:33:31 +0200

    Test Calculator class with unittest.

---
+++ src/test/java/ch/bbw/as/consolecalculator/CalculatorTest.java
@@ -0,0 +1,24 @@
+package ch.bbw.as.consolecalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test Class for Class Calculator
+ * 
+ * 
+ * @author Alex Schaub
+ * @version 0.0.1
+ */
+public class CalculatorTest {
+	Calculator testee;
+	
+	
+	@Test
+	public void testSummeZweiPositiveIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.summe(10, 25) == 35);
+	}
+
+}
