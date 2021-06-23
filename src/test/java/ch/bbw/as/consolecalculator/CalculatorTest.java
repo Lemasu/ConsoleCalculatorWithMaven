2d1a37969c24ea9028aa9611bdbc72aa82dd8dac
Lemasu <alex.schaub@lernende.bbw.ch>
2021-06-23 10:44:51 +0200

    Es soll getestet werden, ob das Programm auch funktioniert, wenn die Summe gleich 0 sein soll.

--- src/test/java/ch/bbw/as/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/as/consolecalculator/CalculatorTest.java
@@ -31,6 +31,11 @@ public class CalculatorTest {
 	}
 
 	@Test
+	public void testSummeGleichNullIsOk() {
+		assertTrue(testee.summe(10, -10) == 0);
+	}
+
+	@Test
 	public void testSubtractionZweiPositiveIsOk() {
 		assertTrue(testee.subtraktion(25, 10) == 15);
 	}
