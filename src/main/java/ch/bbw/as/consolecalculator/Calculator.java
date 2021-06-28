98365c6529538b864f6bde77dbea4e20c1c1047a
Lemasu <alex.schaub@lernende.bbw.ch>
2021-06-28 12:38:35 +0200

    Das Programm soll eine Division mit zwei Integers berechnen können.

--- src/main/java/ch/bbw/as/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/as/consolecalculator/Calculator.java
@@ -32,4 +32,8 @@ public class Calculator {
     public int subtraktion(int value1, int value2) {
         return value1 - value2;
     }
+
+    public int division(int dividend, int divisor) {
+        return dividend / divisor;
+    }
 }
