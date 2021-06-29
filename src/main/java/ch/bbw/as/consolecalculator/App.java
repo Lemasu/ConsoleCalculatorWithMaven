a9d4fa2c375ca91795405930e99d7eefdbe1691c
Lemasu <alex.schaub@lernende.bbw.ch>
2021-06-29 21:42:58 +0200

    Das Programm soll die Subtraktion richtig darstellen.

--- src/main/java/ch/bbw/as/consolecalculator/App.java
+++ src/main/java/ch/bbw/as/consolecalculator/App.java
@@ -21,6 +21,6 @@ public class App
         valueA = 10;
         valueB = 20;
         System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(valueA, valueB));
-        System.out.println("Differenz " + valueA + " + " + valueB + " = " + calculator.differenz(valueA, valueB));
+        System.out.println("Differenz " + valueA + " - " + valueB + " = " + calculator.differenz(valueA, valueB));
     }
 }
